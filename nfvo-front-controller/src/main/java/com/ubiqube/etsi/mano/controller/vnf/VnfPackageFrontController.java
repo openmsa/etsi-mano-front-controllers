d457437c368d9d6742e45739c3e1148d0a583f6b
Olivier Vignaud <ovi@ubiqube.com>
2024-12-31 12:51:16 +0100

    ♻️ Refactor searchable API.

--- nfvo-front-controller/src/main/java/com/ubiqube/etsi/mano/controller/vnf/VnfPackageFrontController.java
+++ nfvo-front-controller/src/main/java/com/ubiqube/etsi/mano/controller/vnf/VnfPackageFrontController.java
@@ -1,18 +1,18 @@
 /**
- *     Copyright (C) 2019-2024 Ubiqube.
+ * Copyright (C) 2019-2024 Ubiqube.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 package com.ubiqube.etsi.mano.controller.vnf;
 
@@ -51,7 +51,7 @@ public interface VnfPackageFrontController {
 
 	ResponseEntity<Void> deleteById(UUID vnfPkgId);
 
-	<U> ResponseEntity<String> search(MultiValueMap<String, String> requestParams, Function<VnfPackage, U> mapper, Consumer<U> makeLinks, Class<?> frontClass);
+	<U> ResponseEntity<String> search(MultiValueMap<String, String> requestParams, Function<VnfPackage, U> mapper, Consumer<U> makeLinks, Class<U> frontClass);
 
 	<U> ResponseEntity<U> create(Map<String, String> userDefinedData, Function<VnfPackage, U> mapper, Consumer<U> makeLinks, Function<U, String> getSelfLink);
 
--- vnfm-front-controller/src/main/java/com/ubiqube/etsi/mano/vnfm/fc/vnflcm/VnfInstanceGenericFrontController.java
+++ vnfm-front-controller/src/main/java/com/ubiqube/etsi/mano/vnfm/fc/vnflcm/VnfInstanceGenericFrontController.java
@@ -1,18 +1,18 @@
 /**
- *     Copyright (C) 2019-2024 Ubiqube.
+ * Copyright (C) 2019-2024 Ubiqube.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 package com.ubiqube.etsi.mano.vnfm.fc.vnflcm;
 
@@ -67,6 +67,6 @@ public interface VnfInstanceGenericFrontController {
 
 	<U> ResponseEntity<U> create(String vnfdId, String vnfInstanceName, @Nullable String vnfInstanceDescription, Function<VnfInstance, U> func, Consumer<U> makeLink, String selfLink);
 
-	<U> ResponseEntity<String> search(MultiValueMap<String, String> requestParams, Function<VnfInstance, U> mapper, @Nullable String nextpageOpaqueMarker, Consumer<U> makeLink, Class<?> frontClass);
+	<U> ResponseEntity<String> search(MultiValueMap<String, String> requestParams, Function<VnfInstance, U> mapper, @Nullable String nextpageOpaqueMarker, Consumer<U> makeLink, Class<U> frontClass);
 
 }
